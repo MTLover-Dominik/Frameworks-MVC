a47bfd41684659a93a62bd5ee81e276f92c7e04f
Dominik Goetz <dominik.goetz@atp-autoteile.de>
2023-09-22 15:13:41 +0200

    added logout route

--- routes/login.js
+++ routes/login.js
@@ -3,11 +3,17 @@ const express = require('express');
 const { registerView, loginView } = require('../controllers/loginController');
 const { registerUser, loginUser } = require('../controllers/loginController');
 const { dashboardView } = require("../controllers/dashboardController");
+const { logoutView } = require("../controllers/logoutController");
 const { protectRoute } = require("../auth/protect");
 const router = express.Router();
 router.get('/register', registerView);
 router.get('/login', loginView);
 router.get("/dashboard", protectRoute, dashboardView);
+router.get('/logout', logoutView);
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/login");
+});
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 module.exports = router;
